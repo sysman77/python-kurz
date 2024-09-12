f85fb69c2d4fe2b9910785d4b8c9f1b2c45de2ae
sysman77 <ales.misurec@gmail.com>
2024-09-12 19:07:04 +0200

    Auth template

---
+++ library/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", home_page, name="home"),
+    path("about/", about_page, name="about"),
+    path("contacts/", contacts_page, name="contacts"),
+]
