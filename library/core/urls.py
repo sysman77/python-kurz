d68451f81420860e9a69a5cf97622b45075b9f3c
sysman77 <ales.misurec@gmail.com>
2024-09-12 20:00:04 +0200

    Login and Logout page

--- library/core/urls.py
+++ library/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home_page, name="home"),
     path("about/", about_page, name="about"),
     path("contacts/", contacts_page, name="contacts"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
